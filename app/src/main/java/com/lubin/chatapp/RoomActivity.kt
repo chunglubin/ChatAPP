79bd4359ff0e8710975b8f18e120ed4bab864484
lubin <lubinforever2020@gmal.com>
2022-03-10 16:15:49 +0800

    all activities for chat APP.

---
+++ app/src/main/java/com/lubin/chatapp/RoomActivity.kt
@@ -0,0 +1,11 @@
+package com.lubin.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RoomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room)
+    }
+}
